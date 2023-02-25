6d80614f899b061fe3ec3ec609a0ac28bcb8f32d
LDR <89.freedom@gmail.com>
2023-02-25 14:48:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const promo = document.getElementsByClassName('promo'),
+      adv = document.getElementsByClassName('promo__adv'),
+      genre = document.getElementsByClassName('promo__genre'),
+      bg = document.querySelectorAll('.promo__bg');
+adv[0].remove();
+genre[0].textContent = 'ДРАМА';
+bg[0].style.cssText = 'background: url(../img/bg.jpg) center top/cover no-repeat;';
