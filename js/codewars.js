30ef0914644c29470fd199543258ce07273d0ed5
LDR <89.freedom@gmail.com>
2023-03-15 00:27:36 +0600

    added a new js file for codewars tasks, and completed a couple tasks

---
+++ js/codewars.js
@@ -0,0 +1,44 @@
+/* 
+// input
+
+const members = [[81, 13], [63, 25], [62, 7], [64, 3], [33, 14], [59, 0], [81, 16]];
+
+//output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data) {
+    const arr = [];
+    data.forEach(function(item, i){
+        if (item[0] >= 55 && item[1] > 7){
+            arr.push("Senior");
+        } else {
+            arr.push("Open");
+        }
+    });
+    return console.log(arr);
+}
+openOrSenior(members); */
+
+/* spinWords("Hey fellow warriors") => returns "Hey wollef sroirraw"
+spinWords("This is a test") => returns "This is a test"
+spinWords("This is another test")=> returns "This is rehtona test" */
+
+
+
+function spinWords(string) {
+    const result = string.split(' ');
+    
+    result.forEach((element, i) => {
+        if (element.length >= 5) {
+            result.splice(i, 1, element.split("").reverse().join(""));
+        }
+    });
+
+    if (result.length > 1) {
+        console.log(result.join(' '));
+    } else {
+        console.log(result.join());
+    }
+
+    
+}
+spinWords("Hey fellow warriors");
